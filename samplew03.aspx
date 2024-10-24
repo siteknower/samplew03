5175600df10844f12a3f308f39a1cf14132cbbfb
siteknower <office@siteknower.com>
2024-10-24 14:26:18 +0200

    Initial commit

---
+++ samplew03.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="samplew03.aspx.cs" Inherits="samplew03" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>samplew03</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+           <div>
+               Customer:
+            </div>
+
+             <asp:TextBox ID="txtCustomer" runat="server"></asp:TextBox>
+
+             <br /><br />
+             <div id="grid-container">
+             <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" AutoGenerateEditButton="false"> 
+                
+                 <Columns>                  
+                     <asp:TemplateField HeaderText="Code" SortExpression="Code">
+                         <ItemStyle Width="150px" /> 
+                         <HeaderStyle Width="150px" /> 
+                         <HeaderStyle HorizontalAlign = "Left" /> 
+                         <ItemTemplate>
+                             <%# Eval("Code") %>
+                         </ItemTemplate>
+                     </asp:TemplateField>
+
+                     <asp:TemplateField HeaderText="Name" SortExpression="Name">
+                         <ItemStyle Width="150px" /> 
+                         <HeaderStyle Width="150px" /> 
+                         <HeaderStyle HorizontalAlign = "Left" /> 
+                         <ItemTemplate>
+                             <%# Eval("Name") %> 
+                          </ItemTemplate>
+                      </asp:TemplateField>
+                                       
+                     <asp:TemplateField HeaderText="Amount" SortExpression="Amount">
+                         <ItemStyle Width="150px" /> 
+                         <ItemStyle HorizontalAlign = "Right" /> 
+                         <HeaderStyle Width="150px" /> 
+                         <HeaderStyle HorizontalAlign = "Right" /> 
+                         <ItemTemplate>
+                             <%# Eval("Amount") %>
+                         </ItemTemplate>                        
+                     </asp:TemplateField>
+
+                      <asp:TemplateField HeaderText="Price" SortExpression="Price">
+                         <ItemStyle Width="150px" /> 
+                         <ItemStyle HorizontalAlign = "Right" /> 
+                         <HeaderStyle Width="150px" /> 
+                         <HeaderStyle HorizontalAlign = "Right" /> 
+                         <ItemTemplate>
+                             <%# Eval("Price") %>
+                         </ItemTemplate>                   
+                     </asp:TemplateField>
+
+                   
+                 </Columns>
+             </asp:GridView>
+         </div>
+
+         <br /><br />
+         <asp:Button ID="Button1" runat="server" Text="Print" OnClick="btnPrint_Click"   />
+         <br /><br />
+         <asp:Button ID="Button2" runat="server" Text="Save schema" OnClick="btnSaveSchema_Click"   />
+
+    </form>
+</body>
+</html>
+
+
+<%--https://www.aspsnippets.com/Articles/1591/GridView-RowUpdating-event-example-in-ASPNet-using-C-and-VBNet/--%>
---
+++ samplew03.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using StnwServiceWeb;
+
+public partial class samplew03 : System.Web.UI.Page
+{
+    public DataSet dst;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Bind the GridView with data from the DataSet
+            dst = GetData();
+            ViewState["ItemsData"] = dst; // Store the dataset in ViewState
+            GridView1.DataSource = dst.Tables["InvItems"];
+            GridView1.DataBind();
+
+            DataTable dtd = dst.Tables["Dummy"];
+            txtCustomer.Text = dtd.Rows[0]["Text1"].ToString();
+        }
+        else
+        {
+            // Retrieve the dataset from ViewState on postback
+            dst = (DataSet)ViewState["ItemsData"];
+        }
+    }
+
+    private DataSet GetData()
+    {
+        DataSet dsInvoice = new DataSet();
+        dsInvoice.Tables.Add(new DataTable("InvItems"));
+        DataTable dt = dsInvoice.Tables["InvItems"];
+        dt.Columns.Add("Id", typeof(string));
+        dt.Columns.Add("Code", typeof(string));
+        dt.Columns.Add("Name", typeof(string));
+        dt.Columns.Add("Amount", typeof(double));
+        dt.Columns.Add("Price", typeof(double));
+        dt.PrimaryKey = new DataColumn[] { dt.Columns["Id"] };
+
+        dsInvoice.Tables.Add(new DataTable("Dummy"));
+        DataTable dtd = dsInvoice.Tables["Dummy"];
+        dtd.Columns.Add("Text1", typeof(string));
+
+        DataRow dr;
+        dr = dt.NewRow();
+        dr["Id"] = Guid.NewGuid().ToString();
+        dr["Code"] = "BB8527";
+        dr["Name"] = "Bread";
+        dr["Amount"] = 1;
+        dr["Price"] = 2.15;
+        dsInvoice.Tables["InvItems"].Rows.Add(dr);
+        dr.EndEdit();
+        dr = dt.NewRow();
+        dr["Id"] = Guid.NewGuid().ToString();
+        dr["Code"] = "SA482";
+        dr["Name"] = "Chocolate";
+        dr["Amount"] = 2;
+        dr["Price"] = 7.65;
+        dsInvoice.Tables["InvItems"].Rows.Add(dr);
+        dr.EndEdit();
+        dr = dt.NewRow();
+        dr["Id"] = Guid.NewGuid().ToString();
+        dr["Code"] = "QCI24";
+        dr["Name"] = "Cheese";
+        dr["Amount"] = 1;
+        dr["Price"] = 2.08;
+        dsInvoice.Tables["InvItems"].Rows.Add(dr);
+        dr.EndEdit();
+        dr = dt.NewRow();
+        dr["Id"] = Guid.NewGuid().ToString();
+        dr["Code"] = "MOX58";
+        dr["Name"] = "Juice";
+        dr["Amount"] = 1;
+        dr["Price"] = 3.55;
+        dsInvoice.Tables["InvItems"].Rows.Add(dr);
+        dr.EndEdit();
+        dr = dt.NewRow();
+        dr["Id"] = Guid.NewGuid().ToString();
+        dr["Code"] = "PB154";
+        dr["Name"] = "Milk";
+        dr["Amount"] = 3;
+        dr["Price"] = 7.87;
+        dsInvoice.Tables["InvItems"].Rows.Add(dr);
+        dr.EndEdit();
+
+        dr = dtd.NewRow();
+        dr["Text1"] = "Phoenixer inc.";
+        dsInvoice.Tables["Dummy"].Rows.Add(dr);
+        dr.EndEdit();
+
+        return dsInvoice;
+    }
+
+    protected void btnPrint_Click(object sender, EventArgs e)
+    {
+
+        clsStnwClassWeb tsi = new clsStnwClassWeb();
+
+        dst.Tables["Dummy"].Rows[0]["Text1"] = txtCustomer.Text;
+
+        tsi.dsRPT = dst;
+        string binPath = HttpContext.Current.Server.MapPath("~/bin");
+        tsi.ReportFullName = System.IO.Path.Combine(binPath, "InvoiceReport.rpt");
+
+        tsi.preslAccountCode = "DEMO1";
+        tsi.preslUserCode = "0000";
+
+        tsi.ShowWindow(this, HttpContext.Current);
+    }
+
+    protected void btnSaveSchema_Click(object sender, EventArgs e)
+    {
+        string schemaXml = "";
+
+        // Generate the XML schema as a string
+        using (StringWriter sw = new System.IO.StringWriter())
+        {
+            using (XmlWriter xmlWriter = XmlWriter.Create(sw))
+            {
+                // Assuming dsInvoice is your DataSet
+                dst.WriteXmlSchema(xmlWriter);
+            }
+            schemaXml = sw.ToString();
+        }
+
+        // Prepare the file for download
+        Response.Clear();
+        Response.ContentType = "application/xml";
+        Response.AddHeader("Content-Disposition", "attachment; filename=schema.xml");
+        Response.Write(schemaXml);
+        Response.End();
+    }
+
+
+    //The simplest example for displaying a Crystal Report from a web page.
+}
